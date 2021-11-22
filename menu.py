aeeaed4c11eafaa232a5cc07759b2a978e9e8a4d
Marcos Madeira <mmadeirasilva5@gmail.com>
2021-11-22 12:40:11 -0300

    Adding necessary files to the script

---
+++ menu.py
@@ -0,0 +1,40 @@
+from rich import print
+from rich.console import Console
+
+
+class Principal():
+    def Menu(self):
+        print('-' * 56)
+        print (f"{'[green]SEG SERVIÇOS EMPRESARIAIS LTDA': >48}")
+        print('-' * 56)
+        print(f"{'MENU PRINCIPAL': ^48}")
+        print('-'* 56)
+        print('[ 1 ] - Entrada de NFS-e')
+        print('[ 2 ] - Lançamento de Retenções NFS-e')
+        print('[ 3 ] - Sair')
+
+        
+    def Choice(self=0):
+        options = int(input('Escolha a opção desejada: '))
+        
+        
+
+
+
+    
+            
+   
+
+    
+
+
+        
+    
+
+
+
+    
+
+
+
+
